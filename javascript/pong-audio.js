6b107e3644eb86e82b9dc271861d5ad70d06518f
DSJ35 <154249811+DSJ35@users.noreply.github.com>
2024-01-01 10:59:46 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,7 +43,7 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("AnimeLoop.mp3"); //load sound
+export var wallSound = new soundFile("Wall Hit.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("NewPaddleSound.mp3");
@@ -62,7 +62,7 @@ soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("newscore.mp3");
+export var villageMusic = new soundFile("AnimeLoop.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
